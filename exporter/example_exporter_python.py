765997ae246e98189d1781c0887acc4981166f22
Joe J Collins <joejcollins@users.noreply.github.com>
2019-07-24 22:11:47 +0100

    Pipfile for exporter

--- exporter/example_exporter_python.py
+++ exporter/example_exporter_python.py
@@ -1,15 +1,16 @@
-from prometheus_client import start_http_server, Summary
+""" Demonstration Exporter """
 import random
 import time
+from prometheus_client import start_http_server, Summary
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
 # Decorate function with metric.
 @REQUEST_TIME.time()
-def process_request(t):
+def process_request(some_time):
     """A dummy function that takes some time."""
-    time.sleep(t)
+    time.sleep(some_time)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
