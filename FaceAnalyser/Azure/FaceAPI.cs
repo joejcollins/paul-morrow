51076092ad9442bfe016fb14a4bb881708643077
Atarad Munir <a.munir@zengenti.com>
2019-07-25 12:23:53 +0100

    FaceAnalyser Project Base

---
+++ FaceAnalyser/Azure/FaceAPI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace FaceAnalyser.Azure
+{
+    public class FaceAPI
+    {
+        public static async void MakeAnalysisRequest(string subscriptionKey, string uriBase)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response;
+
+            // request header
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // request parameters
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // uri for REST API call
+            string uri = uriBase + "?" + requestParameters;
+
+            // request body
+            byte[] byteData = GetImageAsByteArray("imagePathGoesHere");
+
+            // make request
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // set headers
+                content.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/json");
+
+                // execute
+                response = await client.PostAsync(uri, content);
+
+                // get response
+                string contentString = await response.Content.ReadAsStringAsync();
+
+
+            }
+
+                
+        }
+
+        private static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =  new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
---
+++ FaceAnalyser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+namespace FaceAnalyser
+{
+    public class Program
+    {
+        const string subscriptionKey = "84040947493b4fd685ab2c2b5a1d5714";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static void AnalyseImage()
+        {
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
