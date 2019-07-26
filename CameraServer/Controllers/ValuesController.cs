8ad02a0806aa7a5e00e9d8dc3a6598f5bc2a9590
Antony Doyle <a.doyle@zengenti.com>
2019-07-26 09:38:42 +0100

    Finally got the bloody thing running correctly.

--- CameraServer/Controllers/ValuesController.cs
+++ CameraServer/Controllers/ValuesController.cs
@@ -13,31 +13,28 @@ namespace CameraServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class CaptureController : ControllerBase
     {
-        // GET api/values
+        // GET api/capture
         [HttpGet]
         public IActionResult Get()
         {
-
-
             Mat frame;
             Bitmap image = null;
-            Thread camera;
-            int isCameraRunning = 0;
-
-             VideoCapture capture = new VideoCapture(0);
-             frame = new Mat();
-                if (!capture.IsOpened())
-                {
-                    Console.WriteLine("Camera hasn't started yet");
-                }
-                else {
-                    Console.WriteLine("Camera has started");
-                    capture.Read(frame);
-                    image = BitmapConverter.ToBitmap(frame);
-                }
-                image.Save(string.Format(@"capture.png", Guid.NewGuid()), ImageFormat.Png);
+            VideoCapture capture = new VideoCapture(0);
+            capture.Set(CaptureProperty.FrameHeight, 800.0);
+            capture.Set(CaptureProperty.FrameWidth, 1024.0);
+            frame = new Mat();
+            if (!capture.IsOpened())
+            {
+                Console.WriteLine("Camera hasn't started yet");
+            }
+            else {
+                Console.WriteLine("Camera has started");
+                capture.Read(frame);
+                image = BitmapConverter.ToBitmap(frame);
+            }
+            image.Save(string.Format(@"capture.png", Guid.NewGuid()), ImageFormat.Png);
 
         Byte[] b = System.IO.File.ReadAllBytes(@"capture.png");   // You can use your own method over here.         
             return File(b, "image/jpeg");
